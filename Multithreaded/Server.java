4c7befcdd78b9ad0064e7fd146327cc37b3049b4
Shivam <shivamtamboli66@gmail.com>
2026-02-24 00:08:17 +0530

    define getConsumer method that accepts T type but doesnt return anythings

--- Multithreaded/Server.java
+++ Multithreaded/Server.java
@@ -2,8 +2,13 @@ package Multithreaded;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.function.Consumer;
 
 public class Server {
+
+    public Consumer<Socket> getConsumer(){
+        return
+    }
 
     public static void main(String[] args) {
         int port = 8010;
