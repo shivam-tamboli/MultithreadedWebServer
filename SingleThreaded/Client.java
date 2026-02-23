24946aaaeb6ddc8b7983cea5690cf559c9919588
Shivam <shivamtamboli66@gmail.com>
2026-02-23 21:44:10 +0530

    resolved import errors

--- SingleThreaded/Client.java
+++ SingleThreaded/Client.java
@@ -1,8 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
 
-    public void run() throws UnknownHostException{
+    public void run() throws UnknownHostException, IOException{
         int port  = 8010;
-        InetAddress address = InetAddress.getLocalHost("localhost");
+        InetAddress address = InetAddress.getByName("localhost");
+        Socket socket = new Socket(address, port);
+        PrintWriter toSocket = new PrintWriter(socket.getOutputStream());
+        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toSocket.println("Hello from the Client");
 
     }
 
