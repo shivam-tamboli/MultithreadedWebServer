e0ac6c002caa7bea36891484b3753dd77b763e47
Shivam <shivamtamboli66@gmail.com>
2026-02-24 00:26:35 +0530

    multiple threats will be created

--- Multithreaded/Client.java
+++ Multithreaded/Client.java
@@ -4,6 +4,14 @@ public class Client {
 
     public static void main(String[] args) {
         Client client = new Client();
+        for(int i = 0; i<100; i++){
+            try{
+               Thread thread = new Thread(client.getRunnable()); 
+               thread.start();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
 
