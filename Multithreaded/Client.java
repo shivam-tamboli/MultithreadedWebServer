cfcaf8a06178d87af19f03204ce1e9f17f383c2c
Shivam <shivamtamboli66@gmail.com>
2026-02-24 00:27:26 +0530

    define getRunnable method

--- Multithreaded/Client.java
+++ Multithreaded/Client.java
@@ -2,6 +2,10 @@ package Multithreaded;
 
 public class Client {
 
+    public Runnable getRunnable(){
+        
+    }
+
     public static void main(String[] args) {
         Client client = new Client();
         for(int i = 0; i<100; i++){
