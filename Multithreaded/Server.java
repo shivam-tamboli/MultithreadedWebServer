ed56ef629b904a5f7178f8f8be6d2896a5d9f225
Shivam <shivamtamboli66@gmail.com>
2026-02-24 00:05:06 +0530

    initialize the port create socket wait for client

--- Multithreaded/Server.java
+++ Multithreaded/Server.java
@@ -1,7 +1,23 @@
 package Multithreaded;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
+    public static void main(String[] args) {
+        int port = 8010;
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.getSoTimeout(10000);
+            System.out.println("Server is listening on port : " + port);
+            while(true){
+                Socket acceptedSocket = serverSocket.accept();    
+                Thread thread = new Thread();       
+            
+            }
+        }
+    }
     
     
 }
